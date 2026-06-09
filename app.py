2ab3f1598a4ee84203590f94e9ce177f3fd3f8f3
gizemulusoy <gizemulusoy2002@hotmail.com>
2026-06-09 23:06:35 +0300

    Added YOLO object detection and improved color analysis

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from tensorflow.keras.applications.mobilenet_v2 import (
     decode_predictions
 )
 from collections import Counter
+from ultralytics import YOLO
+import cv2
+import math
 
 st.title("FashionInsightAI")
 
@@ -17,53 +20,117 @@ st.write("AI features are currently under development.")
 @st.cache_resource
 def load_model():
     return MobileNetV2(weights="imagenet")
+@st.cache_resource
+def load_detection_model():
+    return YOLO("yolov8n.pt")
 
 model = load_model()
+detection_model = load_detection_model()
 
 # Color Analysis Function
 def get_dominant_color(image):
-    small_image = image.resize((50, 50))
-
+    small_image = image.resize((80, 80))
     pixels = list(small_image.getdata())
+
+    filtered_pixels = []
+
+    for r, g, b in pixels:
+        # white background ignore
+        if r > 220 and g > 220 and b > 220:
+            continue
+
+        # black shadow/background ignore
+        if r < 25 and g < 25 and b < 25:
+            continue
+
+        filtered_pixels.append((r, g, b))
+
+    if len(filtered_pixels) == 0:
+        filtered_pixels = pixels
 
     rounded_pixels = [
         (r // 25 * 25, g // 25 * 25, b // 25 * 25)
-        for r, g, b in pixels
+        for r, g, b in filtered_pixels
     ]
 
     most_common = Counter(rounded_pixels).most_common(1)[0][0]
 
     return most_common
 
+COLOR_MAP = {
+    "Black": (0, 0, 0),
+    "White": (255, 255, 255),
+    "Gray": (128, 128, 128),
+    "Red": (255, 0, 0),
+    "Green": (0, 128, 0),
+    "Blue": (0, 0, 255),
+    "Yellow": (255, 255, 0),
+    "Orange": (255, 165, 0),
+    "Brown": (139, 69, 19),
+    "Dark Brown": (101, 67, 33),
+    "Beige": (245, 245, 220),
+    "Pink": (255, 192, 203),
+    "Purple": (128, 0, 128),
+    "Navy": (0, 0, 128),
+    "Olive": (128, 128, 0)
+}
+
 def get_color_name(rgb):
-    r, g, b = rgb
+    min_distance = float("inf")
+    closest_color = None
 
-    if r > 200 and g > 200 and b > 200:
-        return "White"
+    for color_name, color_rgb in COLOR_MAP.items():
+        distance = math.sqrt(
+            (rgb[0] - color_rgb[0]) ** 2 +
+            (rgb[1] - color_rgb[1]) ** 2 +
+            (rgb[2] - color_rgb[2]) ** 2
+        )
 
-    elif r < 60 and g < 60 and b < 60:
-        return "Black"
+        if distance < min_distance:
+            min_distance = distance
+            closest_color = color_name
 
-    elif r > 180 and g < 100 and b < 100:
-        return "Red"
+    return closest_color
 
-    elif r < 100 and g > 180 and b < 100:
-        return "Green"
+def rgb_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
 
-    elif r < 100 and g < 100 and b > 180:
-        return "Blue"
+ 
+def detect_clothing_area(image):
+    results = detection_model(image)
+    result = results[0]
 
-    elif r > 180 and g > 180 and b < 120:
-        return "Yellow"
+    best_box = None
+    best_confidence = 0
+    best_name = None
 
-    elif r > 150 and g > 100 and b < 80:
-        return "Brown"
+    # target_objects = ["person", "handbag", "tie", "backpack", "suitcase", "shoes", "dress", "skirt", "pants", "shirt", "jacket", "sne"
+    # "boots", "sandals", "socks"]
 
-    elif r > 150 and g > 150 and b > 150:
-        return "Light Gray"
+    target_objects = list(detection_model.names.values())
 
-    else:
-        return "Unknown"
+    for box in result.boxes:
+        class_id = int(box.cls[0])
+        confidence = float(box.conf[0])
+        name = detection_model.names[class_id]
+
+        if name in target_objects and confidence > best_confidence:
+            best_confidence = confidence
+            best_box = box.xyxy[0].tolist()
+            best_name = name
+
+    if best_box is None:
+        return image, None, None, None
+
+    x1, y1, x2, y2 = map(int, best_box)
+
+    clothing_area = image.crop((x1, y1, x2, y2))
+
+    image_with_box = np.array(image).copy()
+
+    cv2.rectangle(image_with_box, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    return Image.fromarray(image_with_box), clothing_area, best_name, best_confidence
 
 uploaded_file = st.file_uploader(
     "Upload a fashion image",
@@ -74,6 +141,11 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
 
     st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    image_with_box, clothing_area, detected_name, detection_confidence = detect_clothing_area(image)
+
+    st.subheader("Detected Clothing Area")
+    st.image(image_with_box, caption="Detected Area", use_container_width=True)
 
     resized_image = image.resize((224, 224))
     image_array = np.array(resized_image)
@@ -90,14 +162,35 @@ if uploaded_file is not None:
         st.write(f"Confidence: {confidence * 100:.2f}%")
         st.write("---")
 
-    dominant_color = get_dominant_color(image)
+    if clothing_area is not None:
+        dominant_color = get_dominant_color(clothing_area)
+        # st.write(f"Detected object: {detected_name}")  sometimes it can show wrong detection, so I hide it for now
+        # st.write(f"Detection confidence: {detection_confidence * 100:.2f}%")
+    else:
+        st.warning("No clothing area detected. Full image was used for color analysis.")
+        dominant_color = get_dominant_color(image)
 
     color_name = get_color_name(dominant_color)
 
     st.subheader("Dominant Color")
 
+    hex_color = rgb_to_hex(dominant_color)
+
     st.write(f"Color: {color_name}")
-    st.write(f"RGB: {dominant_color}")      
+
+    st.markdown(
+        f"""
+        <div style="
+            width: 120px;
+            height: 60px;
+            background-color: {hex_color};
+            border: 2px solid #333;
+            border-radius: 8px;
+            margin-top: 10px;
+        "></div>
+        """,
+        unsafe_allow_html=True
+    )     
 
 
     
